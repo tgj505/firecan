839fcc7c6eefbc3894cfbc13a611323dd5d45938
Tom Johnson <johnson.tom.g@gmail.com>
2023-06-10 00:08:23 -0400

    tidying, linting, formatting

--- firecan/config/config.py
+++ firecan/config/config.py
@@ -2,11 +2,11 @@ import pathlib
 
 
 days_to_scrape = 35
-fm3_types = ['tri', 'apt', 'fwih']
-fb_types = ['hfi', 'tfc', 'sfc', 'fmc', 'cfb', 'ros', 'ft']
-fw_types = ['fdr', 'fwi', 'ffmc', 'dmc', 'dc', 'isi', 'bui', 'dsr']
-maps_to_scrape = ['apt', 'ros', 'fdr']
+fm3_types = ["tri", "apt", "fwih"]
+fb_types = ["hfi", "tfc", "sfc", "fmc", "cfb", "ros", "ft"]
+fw_types = ["fdr", "fwi", "ffmc", "dmc", "dc", "isi", "bui", "dsr"]
+maps_to_scrape = ["apt", "ros", "fdr"]
 ROOT_PATH = pathlib.Path(__file__).absolute().parent.parent
-DATA_PATH = ROOT_PATH / 'data'
-PNG_PATH = DATA_PATH / 'raw_png'
-GIF_PATH = DATA_PATH / 'gifs'
+DATA_PATH = ROOT_PATH / "data"
+PNG_PATH = DATA_PATH / "raw_png"
+GIF_PATH = DATA_PATH / "gifs"
--- firecan/tasks/02_animation/task.py
+++ firecan/tasks/02_animation/task.py
@@ -7,5 +7,6 @@ def main():
     for img_type in tqdm.tqdm(maps_to_scrape):
         animation.make_gif(img_type=img_type)
 
+
 if __name__ == "__main__":
     main()
