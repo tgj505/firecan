d7ff2408376f7bcc82c368e45f84c4963615b187
Tom Johnson <johnson.tom.g@gmail.com>
2023-06-13 21:18:18 -0400

    updating default config

--- firecan/config/config.py
+++ firecan/config/config.py
@@ -1,11 +1,11 @@
 import pathlib
 
 
-days_to_scrape = 40
+days_to_scrape = 28
 fm3_types = ["tri", "apt", "fwih"]
 fb_types = ["hfi", "tfc", "sfc", "fmc", "cfb", "ros", "ft"]
 fw_types = ["fdr", "fwi", "ffmc", "dmc", "dc", "isi", "bui", "dsr"]
-maps_to_scrape = fm3_types + fb_types + fw_types
+maps_to_scrape = ["fwi", "ros", "apt"]
 ROOT_PATH = pathlib.Path(__file__).absolute().parent.parent
 DATA_PATH = ROOT_PATH / "data"
 PNG_PATH = DATA_PATH / "raw_png"
