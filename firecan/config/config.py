acae826b601d50fa7c0c53a0c31bb2659b636f59
Tom Johnson <johnson.tom.g@gmail.com>
2023-06-16 13:16:51 -0400

    adding weather map types

--- firecan/config/config.py
+++ firecan/config/config.py
@@ -5,6 +5,7 @@ days_to_scrape = 28
 fm3_types = ["tri", "apt", "fwih"]
 fb_types = ["hfi", "tfc", "sfc", "fmc", "cfb", "ros", "ft"]
 fw_types = ["fdr", "fwi", "ffmc", "dmc", "dc", "isi", "bui", "dsr"]
+w_types = ["temp", "ws", "adi", "zh", "stab", "precip", "rh", "umix", "vi"]
 maps_to_scrape = ["fwi", "ros", "apt"]
 ROOT_PATH = pathlib.Path(__file__).absolute().parent.parent
 DATA_PATH = ROOT_PATH / "data"
